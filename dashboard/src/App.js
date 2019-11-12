f861eecbd9ece1279c922789d7e571dea6b0b92c
Steve Hartken <steve.hartken@gmail.com>
2019-11-12 15:59:36 -0500

    #5 -- Removed React Branding

--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,9 +13,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ></a>
       </header>
     </div>
   );
