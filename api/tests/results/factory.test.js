00c67c5fd3399fce2908520ddeca5f2852f5bad8
Steve Hartken <steve.hartken@gmail.com>
2019-11-13 11:25:38 -0500

    #7 -- Validate Factory
    Need factory for GET and POST
    
    Creating tests, working out dates
    
    Comitted due to style changes, no semi-colons for line ends, updated prettier styles

--- api/tests/results/factory.test.js
+++ api/tests/results/factory.test.js
@@ -1,22 +1,26 @@
-const tap = require("tap");
-const { factory, createResult } = require("../../routes/results/factory");
+const tap = require('tap')
+const { createResult } = require('../../routes/results/factory')
+const { validate } = require('../../routes/results/validate')
+const { valid } = require('../../utilities/validation')
 
 // builds with no data
-tap.ok(createResult());
+tap.ok(createResult())
 // builds with valid data
-tap.test("Result factory valid", t => {
+tap.test('Result factory valid', t => {
   // @TODO: make this into it's own function. Instead of passing around objects as mocks, want to do some transforms
   // like, test UTF, or test empty findings,
-  const result = createResult();
-  result.id = "2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d";
-  result.status = "Success";
-  result.repo = "test";
-  result.findings[0].type = "sast";
-  result.findings[0].ruleId = "G402";
-  result.findings[0].location.path = "connectors/apigateway.go";
-  result.findings[0].location.positions.begin = { line: 60 };
-  result.findings[0].metadata.description = "TLS InsecureSkipVerify set true.";
-  result.findings[0].metadata.severity = "HIGH";
-  console.log(result);
-  t.end();
-});
+  const result = createResult()
+  result.id = '2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d'
+  result.status = 'Success'
+  result.repo = 'test'
+  result.findings[0].type = 'sast'
+  result.findings[0].ruleId = 'G402'
+  result.findings[0].location.path = 'connectors/apigateway.go'
+  result.findings[0].location.positions.begin = { line: 60 }
+  result.findings[0].metadata.description = 'TLS InsecureSkipVerify set true.'
+  result.findings[0].metadata.severity = 'HIGH'
+  console.log(result)
+  tap.ok(validate(result), 'factory.createResult returns valid types')
+  tap.same(result, valid.single)
+  t.end()
+})
--- api/utilities/validation.js
+++ api/utilities/validation.js
@@ -5,136 +5,140 @@ const patterns = {
   github: /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i,
   ruleId: /^[A-Z][0-9]{3,6}$/,
   punctuation: count =>
-    new RegExp(`^[a-zA-Z0-9 \/\(\)_\,\.\?\-]{0,${count}}$`, "g"),
-  underscore: count => new RegExp(`^[a-zA-Z0-9_\/]{0,${count}}$`, "g"),
-  path: count => new RegExp(`^[a-zA-Z0-9_\/\.]{0,${count}}$`, "g"),
-};
+    new RegExp(`^[a-zA-Z0-9 \/\(\)_\,\.\?\-]{0,${count}}$`, 'g'),
+  underscore: count => new RegExp(`^[a-zA-Z0-9_\/]{0,${count}}$`, 'g'),
+  path: count => new RegExp(`^[a-zA-Z0-9_\/\.]{0,${count}}$`, 'g')
+}
 
 const matches = {
-  safe: "abcdef",
-  alphanumeric: "abcdef123",
-  punctuation: "abcdef123.-?",
-  underscore: "abcdef_",
-  file: "abcdef123/test.json",
-  rule: "G401",
-};
+  safe: 'abcdef',
+  alphanumeric: 'abcdef123',
+  punctuation: 'abcdef123.-?',
+  underscore: 'abcdef_',
+  file: 'abcdef123/test.json',
+  rule: 'G401'
+}
 
 // valid status values
-const status = ["Queued", "In Progress", "Success", "Failure"];
+const status = ['Queued', 'In Progress', 'Success', 'Failure']
 // valid timestamps, 3 year data retention
-const clock = value =>
-  (value > Date.now() / 1000) - 3 * 365 * 24 * 60 * 60 && value < Date.now();
+const clock = value => {
+  const now = Date.now()
+  const retention = now - 3 * 365 * 24 * 60 * 60 * 1000
+  const result = value > retention && value < now
+  return result
+}
 
 // valid result fields
 const fields = {
   result: [
-    "id",
-    "status",
-    "repo",
-    "findings",
-    "queuedAt",
-    "scanningAt",
-    "finishedAt",
+    'id',
+    'status',
+    'repo',
+    'findings',
+    'queuedAt',
+    'scanningAt',
+    'finishedAt'
   ],
-  findings: ["type", "ruleId", "location", "metadata"],
-  location: ["path", "positions"],
-  positions: ["begin", "end"],
-  metadata: ["description", "severity"],
-};
+  findings: ['type', 'ruleId', 'location', 'metadata'],
+  location: ['path', 'positions'],
+  positions: ['begin', 'end'],
+  metadata: ['description', 'severity']
+}
 
 const valid = {
   double: {
-    id: "2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d",
-    status: "Success",
-    repo: "test",
+    id: '2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d',
+    status: 'Success',
+    repo: 'test',
     findings: [
       {
-        type: "sast",
-        ruleId: "G402",
+        type: 'sast',
+        ruleId: 'G402',
         location: {
-          path: "connectors/apigateway.go",
+          path: 'connectors/apigateway.go',
           positions: {
             begin: {
-              line: 60,
-            },
-          },
+              line: 60
+            }
+          }
         },
         metadata: {
-          description: "TLS InsecureSkipVerify set true.",
-          severity: "HIGH",
-        },
+          description: 'TLS InsecureSkipVerify set true.',
+          severity: 'HIGH'
+        }
       },
       {
-        type: "sast",
-        ruleId: "G404",
+        type: 'sast',
+        ruleId: 'G404',
         location: {
-          path: "util/util.go",
+          path: 'util/util.go',
           positions: {
             begin: {
-              line: 32,
-            },
-          },
+              line: 32
+            }
+          }
         },
         metadata: {
           description:
-            "Use of weak random number generator (math/rand instead of crypto/rand)",
-          severity: "HIGH",
-        },
-      },
+            'Use of weak random number generator (math/rand instead of crypto/rand)',
+          severity: 'HIGH'
+        }
+      }
     ],
-    queuedAt: 1573244611,
-    scanningAt: 1573244621,
-    finishedAt: 1573244631,
+    queuedAt: Date.now(),
+    scanningAt: Date.now(),
+    finishedAt: Date.now()
   },
   single: {
-    id: "2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d",
-    status: "Success",
-    repo: "test",
+    id: '2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d',
+    status: 'Success',
+    repo: 'test',
     findings: [
       {
-        type: "sast",
-        ruleId: "G402",
+        type: 'sast',
+        ruleId: 'G402',
         location: {
-          path: "connectors/apigateway.go",
+          path: 'connectors/apigateway.go',
           positions: {
             begin: {
-              line: 60,
-            },
-          },
+              line: 60
+            }
+          }
         },
         metadata: {
-          description: "TLS InsecureSkipVerify set true.",
-          severity: "HIGH",
-        },
-      },
+          description: 'TLS InsecureSkipVerify set true.',
+          severity: 'HIGH'
+        }
+      }
     ],
     queuedAt: 1573244611,
     scanningAt: 1573244621,
-    finishedAt: 1573244631,
+    finishedAt: 1573244631
   },
   noDate: {
-    id: "2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d",
-    status: "Success",
-    repo: "test",
+    id: '2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d',
+    status: 'Success',
+    repo: 'test',
     findings: [
       {
-        type: "sast",
-        ruleId: "G402",
+        type: 'sast',
+        ruleId: 'G402',
         location: {
-          path: "connectors/apigateway.go",
+          path: 'connectors/apigateway.go',
           positions: {
             begin: {
-              line: 60,
-            },
-          },
+              line: 60
+            }
+          }
         },
         metadata: {
-          description: "TLS InsecureSkipVerify set true.",
-          severity: "HIGH",
-        },
-      },
-    ],
-  },
-};
+          description: 'TLS InsecureSkipVerify set true.',
+          severity: 'HIGH'
+        }
+      }
+    ]
+  }
+}
 
-module.exports = { patterns, matches, status, clock, fields, valid };
+module.exports = { patterns, matches, status, clock, fields, valid }
